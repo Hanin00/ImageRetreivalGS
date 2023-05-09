5543a8052c0c237792c5e418ec10005b55b88816
Hanin00 <haeun5419@a.ut.ac.kr>
2023-05-09 07:43:30 +0000

    :sparkles: modify git ignore

---
+++ utils/mkGraphs.py
@@ -0,0 +1,14 @@
+
+
+
+
+'''
+  1. 만들어진 walkset으로 Subgraph를 만들고 해당 서브그래프의 feature를 SUREL을 이용해 만듦
+  2. Targe GEV를 이용해 서브 그래프 생성
+  3. 서브 그래프를 randomwalk를 통해 emb 값 생성
+  4. pair 형태로 만들어 학습
+
+
+
+
+'''
