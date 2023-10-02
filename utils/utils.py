34437db7e8699c62ceb3165e6400723c820d7639
Hanin00 <haeun5419@a.ut.ac.kr>
2023-10-03 00:10:12 +0900

    :sparkles: modify - cuda

--- utils/utils.py
+++ utils/utils.py
@@ -177,7 +177,7 @@ device_cache = None
 def get_device():
     global device_cache
     if device_cache is None:
-        device_cache = torch.device("cuda:0") if torch.cuda.is_available() \
+        device_cache = torch.device("cuda") if torch.cuda.is_available() \
             else torch.device("cpu")
         #device_cache = torch.device("cpu")
     return device_cache
